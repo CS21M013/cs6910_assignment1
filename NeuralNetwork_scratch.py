6831b813cc4f9ec2b916065e50840fb02f57fb18
anur98 <99237580+anur98@users.noreply.github.com>
2022-02-14 23:08:34 +0530

    Update NeuralNetwork_scratch.py

--- NeuralNetwork_scratch.py
+++ NeuralNetwork_scratch.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-#np.random.seed(1)
+import copy
+np.random.seed(3)
 
 
 class NeuralNetwork:
@@ -20,9 +21,9 @@ class NeuralNetwork:
         self.layer_activations.append(activation)
         self.num_of_layers = self.num_of_layers + 1
         if(self.num_of_layers == 1):
-            self.weights[str(1)] = np.random.randn(self.layers[-1], self.ip_layer_size)
+            self.weights[str(1)] = np.random.randn(self.layers[-1], self.ip_layer_size)*0.01
         else:
-            self.weights[str(self.num_of_layers)] = np.random.randn(self.layers[-1], self.layers[-2])
+            self.weights[str(self.num_of_layers)] = np.random.randn(self.layers[-1], self.layers[-2])*0.01
 
         self.biases[str(self.num_of_layers)] = np.zeros((num_of_units, 1))
 
@@ -59,7 +60,7 @@ class NeuralNetwork:
     def forward_prop(self, data):
         #data is of dimension (num_of_parameters x num_of_training_examples(m))
         A_next = data
-        self.cache["A0"] =data
+        self.cache["A0"] = data
         self.cache["Z0"] = data
         for i in range(0,len(self.layers)):
             A = A_next
@@ -70,17 +71,61 @@ class NeuralNetwork:
         return A_next
     
     
-    Modify later 
+    #Modify later 
     def calculate_cost(self, pred, true_label):
         #true and pred both are of the form (num of op layer vals x num of trainging examples)
+        m = pred.shape[1]
         cost = pred*true_label
         cost = np.sum(cost, axis = 0)
         x = np.log(cost + 0.000000000001)
         cost = np.sum(x)
-        return -cost
+        return -cost/m
     
+    # def calculate_cost(self, Y_hat, Y):
+    #     m = Y.shape[1]
+    #     L = -1./m * np.sum(Y * np.log(Y_hat))
+    #     return L
+    
+    def gradient_checking(self, x, y, eps):
+        grads_numerical = {}
+        for i in range(self.num_of_layers):
+            (a0, a1) = self.weights[str(i+1)].shape
+            grads_numerical[str(i+1)] = np.zeros((a0, a1))
+            for j in range(a0):
+                for k in range(a1):
+                    self.weights[str(i+1)][j][k] += eps;
+                    O1 = self.forward_prop(x)
+                    J1 = self.calculate_cost(O1, y)
+                    #print("J1", J1)
+                    self.weights[str(i+1)][j][k] -= 2*eps
+                    O2 = self.forward_prop(x)
+                    J2 = self.calculate_cost(O2, y)
+                    self.weights[str(i+1)][j][k] +=eps
+                    #print("J2", J2)
+                    grad = (J1 - J2)/(2*eps)
+                    grads_numerical[str(i+1)][j][k] = grad
         
-
+        grad_biases_num = {}
+        for i in range(0,self.num_of_layers):
+            
+            (b0, b1) = self.biases[str(i+1)].shape
+            print("bo", b0, "b1", b1)
+            grad_biases_num[str(i+1)] = np.zeros((b0, b1))
+            for j in range(b0):
+                self.biases[str(i+1)][j] += eps;
+                O1 = self.forward_prop(x)
+                J1 = self.calculate_cost(O1, y)
+                #print("J1", J1)
+                self.biases[str(i+1)][j]-= 2*eps
+                O2 = self.forward_prop(x)
+                J2 = self.calculate_cost(O2, y)
+                self.biases[str(i+1)][j] +=eps
+                #print("J2", J2)
+                grad = (J1 - J2)/(2*eps)
+                grad_biases_num[str(i+1)][j] = grad
+            
+        
+        return grads_numerical, grad_biases_num
 
     def back_prop(self, pred, true_label):
         #true and pred both are of the form (num of op layer vals x num of trainging examples)
@@ -96,18 +141,30 @@ class NeuralNetwork:
                 dZ = dA*(self.activation_grad(self.cache["Z" + str(k-1)], self.layer_activations[k-2]))
             self.gradients["dW" + str(k)] = dW
             self.gradients["db" + str(k)] = db
+            
     
-    def train(self, X, y, iterations, learning_rate = 0.01):
+    def train(self, X, y, iterations, learning_rate):
+
         for i in range(iterations):
-            self.cache = {}
-            self.gradients = {}
-            output = self.forward_prop(X)
-            c = self.calculate_cost(output, y)
-            print("cost = ", c)
-            self.back_prop(output, y)
-            for j in range(self.num_of_layers):
-                self.weights[str(j+1)] = self.weights[str(j+1)] - learning_rate*self.gradients["dW" + str(j+1)]
-                self.biases[str(j+1)] = self.biases[str(j+1)] - learning_rate*self.gradients["db" + str(j+1)]
+            for k in range(0,X.shape[1], 100):
+                self.cache = {}
+                self.gradients = {}
+                x_ex = X[:, k:k+100]
+                x_ex = np.reshape(x_ex, (X.shape[0], 100))
+                output = self.forward_prop(x_ex)
+                
+                y_ex = y[: , k:k+100]
+                y_ex = np.reshape(y_ex, (y.shape[0], 100))
+                #print(output.shape, y[: , k].shape)
+                
+                # c = self.calculate_cost(output, y[:, k])
+                # print("cost = ", c, "Iteration", i)
+                self.back_prop(output, y_ex)
+                for j in range(self.num_of_layers):
+                    self.weights[str(j+1)] = self.weights[str(j+1)] - learning_rate*self.gradients["dW" + str(j+1)]
+                    self.biases[str(j+1)] = self.biases[str(j+1)] - learning_rate*self.gradients["db" + str(j+1)]
+                print("completed sgd", k)
+            print(i, "completed")
             
 
 
@@ -146,7 +203,7 @@ dataset = pd.read_csv('archive/fashion-mnist_test.csv')
 x_test = dataset.iloc[:, 1:].values
 y_test = dataset.iloc[:, 0].values
 x_test_flatten = np.reshape(x_test, (10000, 784)).T
-
+x_test_flatten = x_test_flatten/255
 
 # initializing variables
 
@@ -170,18 +227,58 @@ for i in range(num_test_examples):
 
 
 model = NeuralNetwork(784)
-model.add(20, "relu")
-
-
+model.add(512, "relu")
+model.add(256, "relu")
+#model.add(256, "relu")
 model.add(10, "softmax")
-#op = model.forward_prop(x_train_flatten)
+base = model.forward_prop(x_train_flatten)
 
 # cost_train ,x = model.calculate_cost(op, y_train_onehot)
 # print (cost_train)
 # model.back_prop(op, y_train_onehot)
 
-model.train(x_train_flatten, y_train_onehot, 500)
+model.train(x_train_flatten, y_train_onehot, 50, learning_rate=0.001)
 
 op = model.forward_prop(x_train_flatten)
 predicted_value = np.argmax(op, axis = 0)
 print(accuracy(predicted_value, y_train))
+
+
+# model = NeuralNetwork(5)
+# model.add(4, "relu")
+# model.add(3, "relu")
+# model.add(4, "softmax")
+
+# x_data = np.array([[1,6],[2,7],[3,9],[4,0],[5,1]])
+# y_data = np.array([[0,0], [0,0], [1,0], [0,1]])
+
+# op_og = model.forward_prop(x_data)
+
+#model.train(x_data, y_data, 10)
+# op = model.forward_prop(x_data)
+# print(model.calculate_cost(op, y_data))
+# calc_grad, calc_biases = model.gradient_checking(x_data, y_data, 0.001)
+# model.back_prop(op, y_data)
+
+# import copy
+
+# weight_records = []
+# dw_records_bp = []
+# dw_records_num = []
+# for i in range(1000):
+#     model.cache = {}
+#     model.gradients = {}
+#     op = model.forward_prop(x_data)
+#     print(model.calculate_cost(op, y_data))
+#     model.back_prop(op, y_data)
+#     dw_records_bp.append(copy.deepcopy(model.gradients))
+#     calc_grad, calc_biases = model.gradient_checking(x_data, y_data, 0.001)
+#     dw_records_num.append(copy.deepcopy(calc_grad))
+#     dw_records_num.append(copy.deepcopy(calc_biases))
+#     record_w = []
+#     for j in range(model.num_of_layers):
+#         model.weights[str(j+1)] = model.weights[str(j+1)] - 0.01*model.gradients["dW" + str(j+1)]
+#         model.biases[str(j+1)] = model.biases[str(j+1)] - 0.01*model.gradients["db" + str(j+1)]
+#         record_w.append(copy.deepcopy(model.weights[str(j+1)]))
+#         record_w.append(copy.deepcopy(model.biases[str(j+1)]))
+#     weight_records.append(record_w)
